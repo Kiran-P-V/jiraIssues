52c7ba05304c53d1fe4b31bda13be0903a926e9d
Kiran-P-V <kiranpv82@gmail.com>
2024-06-07 12:29:50 +0530

    Testing production

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(corsMiddleware);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 // Routes
 app.use("/jira", router);
 
