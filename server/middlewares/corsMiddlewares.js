0d9e511c94652d95e9d3a07d485a782eccff7c9a
Kiran-P-V <kiranpv82@gmail.com>
2024-06-07 11:36:32 +0530

    Testing production

--- server/middlewares/corsMiddlewares.js
+++ server/middlewares/corsMiddlewares.js
@@ -1,5 +1,8 @@
 import cors from "cors";
-
-const corsMiddleware = cors();
+const corsOptions = {
+  origin: "http://localhost:5173", // Specify the allowed origin
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+const corsMiddleware = cors(corsOptions);
 
 export default corsMiddleware;
