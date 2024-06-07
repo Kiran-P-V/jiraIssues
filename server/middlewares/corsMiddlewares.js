dab6615afdb6546bb991962b6a158c2479cc0c30
Kiran-P-V <kiranpv82@gmail.com>
2024-06-07 16:18:39 +0530

    Testing production

--- server/middlewares/corsMiddlewares.js
+++ server/middlewares/corsMiddlewares.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://jira-issues-teal.vercel.app"],
+  origin: ["http://localhost:5173", "https://jiraissues-4.onrender.com"],
   optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 const corsMiddleware = cors(corsOptions);
